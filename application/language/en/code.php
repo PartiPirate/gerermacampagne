68bf457a611f1359ba1a1600105ec3f66f64f46c
farlistener <contact@levieuxcedric.com>
2017-02-16 18:35:56 +0000

    #9

---
+++ application/language/en/code.php
@@ -0,0 +1,38 @@
+<?php /*
+	Copyright 2016-2017 Cédric Levieux, Parti Pirate
+
+	This file is part of GererMaCampagne.
+
+    GererMaCampagne is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GererMaCampagne is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GererMaCampagne.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+$lang["code_6051"] = "6051 - Matériels (valeur d'utilisation)";
+$lang["code_6060"] = "6060 - Achats de fourniture et de marchandises";
+$lang["code_6132"] = "6132 - location ou mise à disposition immobilière";
+$lang["code_6400"] = "6400 - personnel salarié, y compris charges sociales";
+$lang["code_6210"] = "6210 - personnel intérimaire";
+$lang["code_6211"] = "6211 - personnel mis à disposition";
+$lang["code_6226"] = "6226 - honoraires et conseils en communication";
+$lang["code_6229"] = "6229 - honoraires d'experts comptable";
+$lang["code_6230"] = "6230 - production audiovisuelles (film, DVD), internet";
+$lang["code_6237"] = "6237 - publications, impressions hors R-93";
+$lang["code_6235"] = "6235 - enquêtes et sondages";
+$lang["code_6240"] = "6240 - transports et déplacements";
+$lang["code_6254"] = "6254 - réunions publiques";
+$lang["code_6257"] = "6257 - frais de réception";
+$lang["code_6260"] = "6260 - frais postaux et de distribution";
+$lang["code_6262"] = "6262 - téléphone et télécommunications";
+$lang["code_6280"] = "6280 - frais divers";
+$lang["code_6600"] = "6600 - frais financiers";
+?>
